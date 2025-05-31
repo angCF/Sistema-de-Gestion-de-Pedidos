1988552ab92a222e7165ab08f998a2c9590ae89d
Mauren Rivera Bautista <mauren.rivera.16@hotmail.com>
2025-05-31 00:38:37 -0500

    FIX: Ajustes docker compose

--- Microservicios/orden/start.sh
+++ Microservicios/orden/start.sh
@@ -1,7 +1,20 @@
-#!/bin/ash
-while ! nc -z eureka 8761 ; do
-    echo "Waiting for the Eureka Server"
+#!/bin/sh
+
+# Esperar a que el Gateway esté disponible
+while ! nc -z gateway 8080; do
+    echo "Esperando a Gateway Server..."
     sleep 3
 done
+echo "✅ Gateway Server disponible."
 
-java -jar app.jar
+# Esperar a que la base de datos de órdenes esté disponible
+while ! nc -z mysql-db 3306; do
+    echo "Esperando a MySQL de órdenes..."
+    sleep 3
+done
+echo "✅ MySQL de órdenes disponible."
+
+# Iniciar la app
+echo "🚀 Todos los servicios están disponibles. Iniciando la aplicación de órdenes..."
+exec java -jar /app.jar
+
